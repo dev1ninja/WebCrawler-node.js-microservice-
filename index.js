b3ce1bbb8e27aba45ed13c216eaebc9630f9aef6
dev1ninja <devninja.up@gmail.com>
2021-09-01 01:19:56 +0900

    extract data

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ const mongoose = require('mongoose')
 const axios_file_download = require('./helpers/download')
 const fs = require('fs')
 
-const axios_jar = axios.create({
-    // WARNING: This value will be ignored.
-    jar: new tough.CookieJar(),
-  });
+// const axios = axios.create({
+//     // WARNING: This value will be ignored.
+//     jar: new tough.CookieJar(),
+//   });
    
 
-axiosCookieJarSupport(axios_jar);
+axiosCookieJarSupport(axios);
 
 const cookieJar = new tough.CookieJar();
 
-axios_jar.defaults.jar = cookieJar
+axios.defaults.jar = cookieJar
 
 const urls = {
     host: 'https://pje.tjma.jus.br',
@@ -50,7 +50,7 @@ const defaultHeaders = {
     'cache-control': 'max-age=0', 
 }
 
-Object.assign(axios_jar.defaults.headers, defaultHeaders)
+Object.assign(axios.defaults.headers, defaultHeaders)
 
 function loadJquery(dom){
     delete require.cache[require.resolve('jquery')]
@@ -78,7 +78,7 @@ async function login(){
         data : data
     };
 
-    const request = axios_jar(config)
+    const request = axios(config)
     try{
         const response = await request
         const cookieString = cookieJar.getCookieStringSync(urls.loginUrl)
@@ -91,7 +91,7 @@ async function login(){
     }
 }
 
-async function loadList(){
+async function loadListView(){
     const dom = await JSDOM.fromURL(urls.listViewUrl, {
         referrer: urls.loginUrl,
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
@@ -103,8 +103,100 @@ async function loadList(){
     });
     return dom
 }
+async function downloadFile(dom){
+    $download = document.getElementById('detalheDocumento:download')
+    const downloadurl = await (function(){
+        return new Promise((resolve, reject) => {
+            dom.window.confirm = (text) => {
+                console.log('download confirm:', text)
+            }
+            dom.window.open = (url, title, features) => {
+                console.log(url, title)
+                resolve(url)
+            }
+            $download.click()
+        })
+    })()
+    axios_file_download(url)
+}
 
-async function extractData(detail_url, p_id){
+async function getEvents(dom){
+    const $timelineDiv = document.getElementById('divTimeLine:eventosTimeLineElement')
+    const eventdates = $timelineDiv.querySelectorAll(".media.data")
+    Array.from(eventdates)
+}
+
+async function saveJson2Mongo(data){
+    const JsonData = mongoose.model('JsonData')
+    const jsonData = new JsonData()
+    jsonData.num_process = data.num_process
+    jsonData.json = data
+    try{
+        await jsonData.save()
+        console.log("process saved:", data.num_process)
+    }catch(e){
+        // console.error(e)
+    }
+}
+
+function setFilterProcessId(pId){
+    const inputIds = [
+        "fPP:numeroProcesso:numeroSequencial",
+        "fPP:numeroProcesso:numeroDigitoVerificador",
+        "fPP:numeroProcesso:ano",
+        "fPP:numeroProcesso:ramoJustica",
+        "fPP:numeroProcesso:respectivoTribunal",
+        "fPP:numeroProcesso:numeroOrgaoJustica",
+    ]
+    const reg = /\d+-\d+\.\d+.\d+.\d+.\d+/
+    const matches = pId.match(reg)
+    for(let i = 0; i < inputIds.length; i++){
+        let $input = document.getElementById(inputIds[i])
+        $($input).val(matches[i])
+        console.log(inputIds[i], $($input).val)
+    }
+}
+
+async function pressSearchBtn(){
+    do{
+        await timer(1000)
+    }while(!window.executarReCaptcha)
+    // console.log()
+    console.log(document.getElementById("fPP:searchProcessos").outerHTML)
+    document.getElementById("fPP:searchProcessos").click()
+    await timer(500)
+}
+
+async function waitLoading(){
+    const $status = document.getElementById('_viewRoot:status.start')
+    do{
+        await timer(1000)
+        console.log("loading..")
+    }while($($status).css('display') != 'none')
+}
+
+function findLinkElement(){
+    const $table = document.getElementById('fPP:processosTable:tb')
+    const $as = Array.from($($table).find('tr>td:first-child')).map( $td => $td.querySelector('a') )
+    console.log("process ids", $as.map($a => $a.id))
+    return $as[0]
+}
+
+async function getProcessDetailUrl($a){
+    return new Promise(resolve => {
+        dom.window.confirm = (text) => {
+            console.log("confirm", text)
+            return true
+        }
+        dom.window.open = (url, title, features) => {
+            // console.log("open new url", url)
+            resolve(url)
+        }
+        $a.click()
+    })
+}
+
+async function getProcessDetail(detail_url, p_id){
     p_id = p_id.split(":")[2]
     const dom = await JSDOM.fromURL(detail_url,
         {
@@ -121,119 +213,66 @@ async function extractData(detail_url, p_id){
     jsondata.num_process = p_id
 
     /****** get details *******/
-    fs.writeFileSync("1.html", dom.serialize())
     loadJquery(dom)
     $details = document.getElementById('maisDetalhes')
     $dts = $details.querySelectorAll('dt')
-    console.log($details, Array.from($dts).length)
+    console.log($details.id, Array.from($dts).map($dt => $($dt).text().trim()))
     let details = Array.from($dts).map($dt => 
         ({
             key: $($dt).text().trim(),
             value: $($dt.nextElementSibling).text().trim()
         })
     )
-    console.log(details)
+    // console.log(details)
+    jsondata.details = details
+
+    /******** polo ********/
+    jsondata.polo_active = getPolo('Ativo')
+    jsondata.polo_passive = getPolo('Passivo')
+    // jsondata.polo_passive = polo_passive
     return jsondata
 }
 
-async function saveJson2Mongo(data){
-    const JsonData = mongoose.model('JsonData')
-    const jsonData = new JsonData()
-    jsonData.num_process = data.num_process
-    jsonData.json = data
-    try{
-        await jsonData.save()
-        console.log("process saved:", data.num_process)
-    }catch(e){
-        // console.error(e)
+function getPolo(type){
+    $poloDiv = document.getElementById(`polo${type}`)
+    $poloDiv = $poloDiv.querySelector('tbody tr:first-child td:first-child')
+    let $firstline = $poloDiv.querySelector('span')
+    let $parts = $poloDiv.querySelectorAll('ul li')
+    console.log(Array.from($parts).map(each => $(each).text().trim()))
+    let firstline = $($firstline).text().trim()
+    let parts = Array.from($parts).map(each => $(each).text().trim())
+    let reg1 = /(.*) - (.*): (.*) \((.*)\)/, reg2 = /(.*) \((.*)\)/
+    let matches = firstline.match(reg1)
+    let polo = {
+        "name": matches[1], 
+        [matches[2]]: matches[3],  //CNPJ
+        "type": matches[4],
+        "parts": parts.map(part => part.match(reg2)).map(
+            matches => ({
+                name: matches[1],
+                type: matches[2]
+            })
+        )
     }
+    return polo
 }
 
 void async function main(){
     await login()
 
-    const dom = await loadList()
+    const dom = await loadListView()
     loadJquery(dom)
-    do{
-        await timer(1000)
-    }while(!window.executarReCaptcha)
-    // console.log()
-    console.log(document.getElementById("fPP:searchProcessos").outerHTML)
-    document.getElementById("fPP:searchProcessos").click()
-
-    await timer(500)
-    const $status = document.getElementById('_viewRoot:status.start')
-    do{
-        await timer(1000)
-        console.log("loading..")
-    }while($($status).css('display') != 'none')
     
-    const $table = document.getElementById('fPP:processosTable:tb')
-    const $as = Array.from($($table).find('tr>td:first-child')).map( $td => $td.querySelector('a') )
-    console.log("process ids", $as)
-    for( let $a of $as ){
-        const p_id = $a.id
-        console.log($a)
-        const detail_url = await (async function(){
-            return new Promise(resolve => {
-                dom.window.confirm = (text) => {
-                    console.log("confirm", text)
-                    return true
-                }
-                dom.window.open = (url, title, features) => {
-                    console.log("open new url", url)
-                    resolve(url)
-                }
-                $a.click()
-            })
-        })()
-        await timer(500)
-        const jsondata = await extractData(detail_url, p_id)
-    }
+    setFilterProcessId('0800097-04.2017.8.10.0135')
+    await pressSearchBtn()
+    await waitLoading()
+    const $a = findLinkElement()
+    const p_id = $a.id
+    console.log("process_id:", p_id)
+    const detail_url = await getProcessDetailUrl($a)
+    await timer(500)
+    
+    const jsondata = await getProcessDetail(detail_url, p_id)
+    console.log(jsondata)
 }()
 
-const testdata = {
-    "num_process": "0800097-04.2017.8.10.0135",
-    "details": [
-        { "key": "Classe judicial", "value": "EXECUÇÃO DE TÍTULO EXTRAJUDICIAL (159)" },
-        { "key": "Jurisdição", "value": "Fórum da Comarca de Tuntum" },
-        { "key": "Valor da causa", "value": "R$ 1.916,50" },
-    ],
-    "polo_active": {
-        "name": "AUDIOLAR MOVEIS E ELETROS LTDA", 
-        "CNPJ": "11.828.573/0001-24", 
-        "type": "EXEQUENTE",
-        "parts": [
-            {"name": "GEORGE MUNIZ RIBEIRO REIS", "type": "ADVOGADO" },
-            {"name": "SARA MANUELE COSTA DOS REIS", "type": "ADVOGADO" }
-        ]
-    },
-    "polo_passivo": {
-        "name": "ADRIANO HENRIQUE ANDRADE SANTOS - ME", 
-        "CNPJ": "24.563.772/0001-08", 
-        "type": "EXECUTADO",
-        "parts": []
-    },
-    "events": [
-        { 
-            "date": "07/08/2021 03:06", 
-            "description": "DECORRIDO PRAZO DE ADRIANO HENRIQUE ANDRADE SANTOS - ME EM 21/07/2021 23:59.",
-            "items": []
-        },
-        { 
-            "date": "15/08/2021 16:57", 
-            "description": "",
-            "items": [
-                { 
-                    "number": "49137915", 
-                    "title": "Petição", 
-                    file: "", 
-                    childs: [
-                        { "number": "49137917", "title": "Petição (Petição Habilitação Processo)", file: "/asdasd.pdf" },
-                        { "number": "49137919", "title": "Procuração (Doc. 01 Procuração Audiolar Moveis Atualizada)", file: "/asdasd.pdf" },
-                    ]						
-                }
-            ]
-        }
-    ]
-}
