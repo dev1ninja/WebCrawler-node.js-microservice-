6b1166daa6ce0b5245665a260618b22a75db5767
dev1ninja <devninja.up@gmail.com>
2021-08-31 21:07:38 +0900

    mongoose, dt

--- index.js
+++ index.js
@@ -11,18 +11,28 @@ const tough = require('tough-cookie')
 const util = require('util')
 const timer = util.promisify(setTimeout)
 require('./database/connect')
+const mongoose = require('mongoose')
+const axios_file_download = require('./helpers/download')
+const fs = require('fs')
 
-axiosCookieJarSupport(axios);
+const axios_jar = axios.create({
+    // WARNING: This value will be ignored.
+    jar: new tough.CookieJar(),
+  });
+   
+
+axiosCookieJarSupport(axios_jar);
 
 const cookieJar = new tough.CookieJar();
 
-axios.defaults.jar = cookieJar
+axios_jar.defaults.jar = cookieJar
 
 const urls = {
     host: 'https://pje.tjma.jus.br',
     loginPage: 'https://pje.tjma.jus.br/pje/login.seam',
     loginUrl: 'https://pje.tjma.jus.br/pje/logar.seam',
-    listViewUrl: 'https://pje.tjma.jus.br/pje/Processo/ConsultaProcesso/listView.seam'
+    listViewUrl: 'https://pje.tjma.jus.br/pje/Processo/ConsultaProcesso/listView.seam',
+    detailViewUrl: 'https://pje.tjma.jus.br/pje/Processo/ConsultaProcesso/Detalhe/listProcessoCompletoAdvogado.seam'
 }
 
 const defaultHeaders = {
@@ -40,10 +50,14 @@ const defaultHeaders = {
     'cache-control': 'max-age=0', 
 }
 
-Object.assign(axios.defaults.headers, defaultHeaders)
+Object.assign(axios_jar.defaults.headers, defaultHeaders)
 
 function loadJquery(dom){
-    global.$ = dom.$
+    delete require.cache[require.resolve('jquery')]
+    global.window = dom.window
+    global.document = dom.window.document
+    global.$ = require('jquery')
+    // console.log($)
 }
 
 async function login(){
@@ -53,6 +67,7 @@ async function login(){
         'newPassword1': '',
         'newPassword2': '' 
     });
+    
     const config = {
         method: 'post',
         url: urls.loginUrl,
@@ -63,7 +78,7 @@ async function login(){
         data : data
     };
 
-    const request = axios(config)
+    const request = axios_jar(config)
     try{
         const response = await request
         const cookieString = cookieJar.getCookieStringSync(urls.loginUrl)
@@ -71,7 +86,7 @@ async function login(){
         console.log('Cookie:', cookieString)
         return cookieString
     }catch(e){
-        console.log(e)
+        // console.log(e)
         return false
     }
 }
@@ -86,18 +101,139 @@ async function loadList(){
         resources: "usable",
         cookieJar
     });
-    loadJquery(dom)
-    // console.log()
-    console.log($("#fPP:searchProcessos").attr('id'))
-    $("#fPP:searchProcessos").click()
-    do{
-        await timer(100)
-    }while($("#xxx").attr('hidden') != true)
+    return dom
+}
 
+async function extractData(detail_url, p_id){
+    p_id = p_id.split(":")[2]
+    const dom = await JSDOM.fromURL(detail_url,
+        {
+            referrer: urls.loginUrl,
+            userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+            includeNodeLocations: true,
+            storageQuota: 10000000,
+            runScripts: 'dangerously',
+            resources: "usable",
+            cookieJar
+        }    
+    )
+    const jsondata = {}
+    jsondata.num_process = p_id
+
+    /****** get details *******/
+    fs.writeFileSync("1.html", dom.serialize())
+    loadJquery(dom)
+    $details = document.getElementById('maisDetalhes')
+    $dts = $details.querySelectorAll('dt')
+    console.log($details, Array.from($dts).length)
+    let details = Array.from($dts).map($dt => 
+        ({
+            key: $($dt).text().trim(),
+            value: $($dt.nextElementSibling).text().trim()
+        })
+    )
+    console.log(details)
+    return jsondata
+}
+
+async function saveJson2Mongo(data){
+    const JsonData = mongoose.model('JsonData')
+    const jsonData = new JsonData()
+    jsonData.num_process = data.num_process
+    jsonData.json = data
+    try{
+        await jsonData.save()
+        console.log("process saved:", data.num_process)
+    }catch(e){
+        // console.error(e)
+    }
 }
 
 void async function main(){
-    // const cookieJar = new jsdom.CookieJar(store, options);
     await login()
-    await loadList()
+
+    const dom = await loadList()
+    loadJquery(dom)
+    do{
+        await timer(1000)
+    }while(!window.executarReCaptcha)
+    // console.log()
+    console.log(document.getElementById("fPP:searchProcessos").outerHTML)
+    document.getElementById("fPP:searchProcessos").click()
+
+    await timer(500)
+    const $status = document.getElementById('_viewRoot:status.start')
+    do{
+        await timer(1000)
+        console.log("loading..")
+    }while($($status).css('display') != 'none')
+    
+    const $table = document.getElementById('fPP:processosTable:tb')
+    const $as = Array.from($($table).find('tr>td:first-child')).map( $td => $td.querySelector('a') )
+    console.log("process ids", $as)
+    for( let $a of $as ){
+        const p_id = $a.id
+        console.log($a)
+        const detail_url = await (async function(){
+            return new Promise(resolve => {
+                dom.window.confirm = (text) => {
+                    console.log("confirm", text)
+                    return true
+                }
+                dom.window.open = (url, title, features) => {
+                    console.log("open new url", url)
+                    resolve(url)
+                }
+                $a.click()
+            })
+        })()
+        await timer(500)
+        const jsondata = await extractData(detail_url, p_id)
+    }
 }()
+
+const testdata = {
+    "num_process": "0800097-04.2017.8.10.0135",
+    "details": [
+        { "key": "Classe judicial", "value": "EXECUÇÃO DE TÍTULO EXTRAJUDICIAL (159)" },
+        { "key": "Jurisdição", "value": "Fórum da Comarca de Tuntum" },
+        { "key": "Valor da causa", "value": "R$ 1.916,50" },
+    ],
+    "polo_active": {
+        "name": "AUDIOLAR MOVEIS E ELETROS LTDA", 
+        "CNPJ": "11.828.573/0001-24", 
+        "type": "EXEQUENTE",
+        "parts": [
+            {"name": "GEORGE MUNIZ RIBEIRO REIS", "type": "ADVOGADO" },
+            {"name": "SARA MANUELE COSTA DOS REIS", "type": "ADVOGADO" }
+        ]
+    },
+    "polo_passivo": {
+        "name": "ADRIANO HENRIQUE ANDRADE SANTOS - ME", 
+        "CNPJ": "24.563.772/0001-08", 
+        "type": "EXECUTADO",
+        "parts": []
+    },
+    "events": [
+        { 
+            "date": "07/08/2021 03:06", 
+            "description": "DECORRIDO PRAZO DE ADRIANO HENRIQUE ANDRADE SANTOS - ME EM 21/07/2021 23:59.",
+            "items": []
+        },
+        { 
+            "date": "15/08/2021 16:57", 
+            "description": "",
+            "items": [
+                { 
+                    "number": "49137915", 
+                    "title": "Petição", 
+                    file: "", 
+                    childs: [
+                        { "number": "49137917", "title": "Petição (Petição Habilitação Processo)", file: "/asdasd.pdf" },
+                        { "number": "49137919", "title": "Procuração (Doc. 01 Procuração Audiolar Moveis Atualizada)", file: "/asdasd.pdf" },
+                    ]						
+                }
+            ]
+        }
+    ]
+}
